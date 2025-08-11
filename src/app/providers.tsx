5d7954740645cae74beaa5406f7b81de129b35df
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2025-08-11 17:15:41 -0500

    Include credentials in tRPC client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,6 +6,16 @@ import superjson from "superjson";
 import { api } from "@/server/api/react";
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
-  const [trpcClient] = React.useState(() => api.createClient({ links: [ httpBatchLink({ url: "/api/trpc", transformer: superjson }) ] }));
+  const [trpcClient] = React.useState(() =>
+    api.createClient({
+      transformer: superjson,
+      links: [
+        httpBatchLink({
+          url: "/api/trpc",
+          fetch: (url, opts) => fetch(url, { ...opts, credentials: "include" }),
+        }),
+      ],
+    })
+  );
   return (<api.Provider client={trpcClient} queryClient={queryClient}><QueryClientProvider client={queryClient}>{children}</QueryClientProvider></api.Provider>);
 }
