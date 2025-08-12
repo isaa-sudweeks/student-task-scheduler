dc259d61b311003438a3112819e8d2c9cf878114
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2025-08-12 13:05:40 -0500

    feat: add theme toggle for light and dark modes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
+import { ThemeProvider } from "next-themes";
 import { api } from "@/server/api/react";
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
@@ -19,10 +20,10 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     })
   );
   return (
-    <api.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </api.Provider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <api.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </api.Provider>
+    </ThemeProvider>
   );
 }
