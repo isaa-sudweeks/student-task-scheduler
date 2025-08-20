821390b39c55bca4b33cd55b810ab6d4d85619d9
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2025-08-19 20:35:18 -0600

    style: format app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,5 +2,11 @@ import { router } from './trpc';
 import { taskRouter } from './routers/task';
 import { eventRouter } from './routers/event';
 import { focusRouter } from './routers/focus';
-export const appRouter=router({task:taskRouter, event:eventRouter, focus:focusRouter});
-export type AppRouter=typeof appRouter;
+
+export const appRouter = router({
+  task: taskRouter,
+  event: eventRouter,
+  focus: focusRouter,
+});
+
+export type AppRouter = typeof appRouter;
